d853e385b9b13cc444d126ce5b6521cfb42be645
Oscar Aviles-Saldana <oaviles-saldana@csumb.edu>
2026-02-08 22:56:22 -0800

    hw

---
+++ Lab2/js/script.js
@@ -0,0 +1,11 @@
+document.querySelector("#guessBtn").addEventListener("click", guess)\
+let userGuess = document.query
+let randomNUmber = Math.floor(Math.random()*99)+ 1;
+
+function guess()
+{
+let userGuess = document.querySelector("#userGuesses").value;
+
+document.querySelector("#userGuesses")
+
+}
