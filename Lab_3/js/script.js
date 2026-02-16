3e7bc93d9096f7f347333a8a28ca055289f28a02
Oscar Aviles-Saldana <oaviles-saldana@csumb.edu>
2026-02-15 23:49:02 -0800

    renew

--- Lab_3/js/script.js
+++ Lab_3/js/script.js
@@ -1,30 +1,107 @@
-//event listner
-document.querySelector("button").addEventListener("click", gradeQuiz);
+document.querySelector("button").addEventListener("click", checkQuestions);
 
+let count = 0;
 shuffleQ1Choices();
-
-function shuffleQ1Choices()
-{
-    let q1Choices = ["font-color", "color", "text-color", "fontColor"]
+function shuffleQ1Choices() {
+    let q1Choices = ["purple", "yellow", "white", "red"];
     q1Choices = _.shuffle(q1Choices);
     console.log(q1Choices);
-    let radioElement= document.createElement("input");
-    radioElement.type = "radio";
-    radioElement.name = "q1";
-    radioElement.value = q1Choices(0);
-    let labelElement = document.createElement("label");
-    labelElement
 
-    console.log(radioElement)
+    for (let i of q1Choices) {
 
+
+
+        let radioElement = document.createElement("input");
+
+        radioElement.type = "radio";
+        radioElement.name = "q1";
+        radioElement.value = i;
+
+        let labelElement = document.createElement("label");
+        labelElement.textContent = i;
+        labelElement.append(radioElement);
+
+        document.querySelector("#q1ChoicesDiv").append(labelElement);
+
+        console.log(labelElement);
+    }
 }
-function gradeQuiz()
-{
-    let userAnswer1 = document.querySelector("input[name=q1]: checking")
-    alert("Grading ")
 
-    if(userAnswer1 == "color")
+
+function checkQuestions() {
+    // alert(`${userChoice}`);
+    checkQ1();
+    checkQ2();
+    checkQ3();
+    checkQ4();
+    checkQ5(); 
+
+    document.querySelector("#score").textContent = `score ${count}`;
+}
+
+function checkQ1(){
+    let userChoice = document.querySelector("input[name=q1]:checked").value;
+    if(userChoice == "purple"){
+        document.querySelector("#q1Text").style.color = "green";
+        count += 20;
+    }else{
+        document.querySelector("#q1Text").style.color = "red";
+    }
+}
+
+function checkQ2(){
+    let userChoice = document.querySelector("#q2Input").value;
+    if(userChoice == "spell"){
+        document.querySelector("#q2Text").style.color = "green";
+        count += 20;
+    }else{
+        document.querySelector("#q2Text").style.color = "red";
+    }
+}
+
+function checkQ3(){
+    let userChoice = document.querySelector("#dropdown").value;
+    if(userChoice == "false"){
+        document.querySelector("#q3Text").style.color = "green";
+        count += 20;
+    }else{
+        document.querySelector("#q3Text").style.color = "red";
+    }
+}
+function checkQ4(){
+    let userChoice = document.querySelector("#numberInput").value;
+    // console.log(userChoice);
+    if(userChoice == 3)
     {
+        document.querySelector("#q4Text").style.color = "green";
+        count += 20;
+    }
+    else{
+        document.querySelector("#q4Text").style.color = "red";
+    }
+}
+function checkQ5()
+{
+    let userChoice1 = document.querySelector("#chbxorg").value;
+    let userChoice2 = document.querySelector("#chbxble").value;
+    let userChoice3 = document.querySelector("#chbxyllw").value;
+    let userChoice4 = document.querySelector("#chbxsun").value;
+    // if(userChoice1 == "true" ){
+    //     document.querySelector("#q5Text").style.color = "red";
+    // }
+    // console.log(document.getElementById("#chxorg"));
+    console.log(userChoice4);
+    console.log(userChoice3);
+    console.log(userChoice2);
 
+    console.log(document.querySelector("chxorg").checked)
+    console.log(document.getElementById("chxble").checked)
+
+
+    if(!document.getElementById("chxorg").checked && document.getElementById("chxble").checked && document.getElementById("chxyllw").checked && !document.getElementById("chxsun").checked){
+        document.querySelector("#q5Text").style.color = "green";
+        count += 20;
+    }else{
+        document.querySelector("#q5Text").style.color = "red";
     }
 }
